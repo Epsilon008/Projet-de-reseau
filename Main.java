eeb9022ce95aab53635fb9fd8bdc0983e63c3de2
Epsilon008 <149121257+Epsilon008@users.noreply.github.com>
2023-12-30 09:50:12 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,107 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+class Taxi {
+    List<String> table_destination;
+    int nombre_place_disponible;
+    List<String> Cp;
+
+    public Taxi(int placesDisponibles) {
+        this.nombre_place_disponible = placesDisponibles;
+        this.table_destination = new ArrayList<>();
+        this.Cp = new ArrayList<>();
+    }
+
+    public String choixClient(String Sn, String Sm, int Pi, int Ni) {
+        List<String> chemin = calculDjikstra(Sn, Sm);
+        if ((double) Pi / Ni >= calculerPrix(chemin)) {
+            nombre_place_disponible -= Ni;
+            table_destination.add(chemin.toString());
+            Cp.add(Sm);
+            return "Accepte";
+        } else {
+            return "Refuse";
+        }
+    }
+
+    private double calculerPrix(List<String> chemin) {
+        return 0;
+    }
+
+    // Logique pour décider d'accepter ou de refuser un client et mettre à jour
+    // table_destination, nombre_place_disponible, etc.
+    private List<String> calculDjikstra(String sn, String sm) {
+        return null;
+    }
+
+    public void choixClientSuivant() {
+        // Logique pour choisir le prochain client à prendre en charge en fonction de
+        // l'algorithme décrit
+    }
+
+    public void depotClient(String destination) {
+        // Logique pour déposer un client à sa destination et mettre à jour les
+        // attributs nécessaires
+        for (String cpi : Cp) {
+            if (Si.equals(cpi)) {
+                // Supprimer cpi de Cp
+                Cp.remove(cpi);
+                // Supprimer Djikstra(., cpi) de tableDestination
+                table_destination.remove(cpi);
+                // Mise à jour de nombrePlaceDisponible
+                nombre_place_disponible += getNombrePlacesClient(cpi); // Obtenez le nombre de places du client cpi
+            }
+        }
+    }
+
+    private int getNombrePlacesClient(String cpi) {
+        return 0;
+    }
+
+    // D'autres méthodes et logiques nécessaires
+}
+
+class Client {
+    String pointDepart;
+    String destination;
+    int nombrePlacesVoulu;
+    int prixPropose;
+
+    public Client(String pointDepart, String destination, int nombrePlacesVoulu, int prixPropose) {
+        this.pointDepart = pointDepart;
+        this.destination = destination;
+        this.nombrePlacesVoulu = nombrePlacesVoulu;
+        this.prixPropose = prixPropose;
+    }
+
+    // Getters et setters pour les attributs
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Taxi taxi = new Taxi(4); // Créer une instance de Taxi avec 4 places disponibles
+        int n = 5;
+        Set<String> sommet = new HashSet<>();
+        for (int i = 1; i <= n; i++) {
+            sommet.add("S" + i);
+        }
+
+        // Ajouter des clients avec leurs détails
+        Client client1 = new Client("A", "B", 2, 20);
+        Client client2 = new Client("C", "D", 3, 25);
+
+        // Gérer l'interaction avec les clients
+        taxi.choixClient("A", "B", 2, 20); // Appliquer votre logique pour choisir d'accepter ou de refuser ce client
+        taxi.choixClient("C", "D", 3, 25); // Appliquer votre logique pour choisir d'accepter ou de refuser ce client
+
+        // Appel des méthodes pour gérer les clients suivants selon votre algorithme
+        taxi.choixClientSuivant();
+        taxi.choixClientSuivant();
+
+        // Déposer un client à sa destination
+        taxi.depotClient("B");
+        taxi.depotClient("D");
+    }
+}
